3b2346c43b1fa509956abacae65f94321b796678
SergeiDegtyarev <degtiarev94@yandex.ru>
2023-01-10 16:11:26 +0400

    g

---
+++ src/main/java/com/example/api/ApiApplication.java
@@ -0,0 +1,25 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+
+
+        Flux<String> flux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next("3 x " + state + " = " + 3*state);
+                    if (state == 10) sink.complete();
+                    return state + 1;
+                });
+        flux.log().subscribe();
+    }
+
+}
---
+++ src/main/java/com/example/api/controller/ExampleController.java
@@ -0,0 +1,27 @@
+package com.example.api.controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class ExampleController {
+
+    @ApiResponse(code = 200,message = "Успешно")
+    @GetMapping
+    public ResponseEntity <String> getText (){
+        log.info("Охуенно");
+        return  new ResponseEntity<>("Pizda", HttpStatus.OK);
+    }
+
+}
